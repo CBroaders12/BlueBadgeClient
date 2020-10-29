67ec27fde724c108ce783f3629fa6db2b8aef5d6
JVinson813 <JVinson813@gmail.com>
2020-10-29 13:19:43 -0400

    Josh

--- src/components/app/FoodTable.jsx
+++ src/components/app/FoodTable.jsx
@@ -1,12 +1,45 @@
-import React,  {  } from 'react';
+import React from 'react';
+import {Table} from 'reactstrap';
+
 
 const FoodTableComponent = (props) => {
 
+  const fetchFoodLog = (food) => {
+    
+    fetch(`http://localhost:5200/food/`, {
+      method: 'GET',
+      headers: new Headers ({
+        'Content-Type': 'application/json',
+        'Authorization': props.token
+      })
+    }).then((res) => res.text())
+      .then(res => console.log(res))
+
+      
+  }
+
+  const foodMapper = () => {
+ 
+  }
+
   return(
-    <div>
-      See Food Here
-    </div>
+    <>
+    <h2>Food History</h2>
+    <hr/>
+    <Table striped>
+      <thead>
+        <tr>
+        
+        </tr>
+      </thead>
+      <tbody>
+        {foodMapper()}
+      </tbody>
+    </Table>
+    </>
   );
 };
+
+
 
 export default FoodTableComponent;
